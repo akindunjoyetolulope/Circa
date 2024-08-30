a9508abc9e3fa50f14bf86c5cc1d1e5a21345465
Akindunjoye <tolulope@busha.co>
2024-08-30 14:49:34 +0100

    Initial Commit

---
+++ Circa/CircaApp.swift
@@ -0,0 +1,17 @@
+//
+//  CircaApp.swift
+//  Circa
+//
+//  Created by Busha Digital Ltd on 30/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct CircaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
