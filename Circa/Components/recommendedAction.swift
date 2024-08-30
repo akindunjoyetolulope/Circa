4b3f0b0ea4bd80b42c0eb5bf8dd6813efe1096b8
Akindunjoye <tolulope@busha.co>
2024-08-30 15:18:49 +0100

    added component folder

---
+++ Circa/Components/recommendedAction.swift
@@ -0,0 +1,18 @@
+//
+//  recommendedAction.swift
+//  Circa
+//
+//  Created by Busha Digital Ltd on 30/08/2024.
+//
+
+import SwiftUI
+
+struct recommendedAction: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    recommendedAction()
+}
