32610af963040e54f41d5a3f534ff77beb199b21
Akindunjoye <tolulope@busha.co>
2024-09-03 21:16:41 +0100

    overview

--- Circa/Components/Overview.swift
+++ Circa/Components/Overview.swift
@@ -8,9 +8,52 @@
 import SwiftUI
 
 struct Overview: View {
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack(){
+            VStack() {
+                VStack(alignment: .leading){
+                    
+                    Text("Weekly Overview")
+                        .multilineTextAlignment(.leading)
+                        .foregroundColor(.white)
+                        .bold()
+                    
+//                    BarchartView()
+                }
+                .padding()
+
+                Divider().background(.gray)
+                
+                HStack(){
+                    HStack(alignment: .top){
+                        Image(systemName: "clock.fill")
+                            .foregroundColor(.white)
+                        VStack(alignment: .leading, spacing: 2){
+                            Text("You averaged 4hr 44min in bed")
+                                .font(.custom("FKGroteskNeueTrial-Regular", size: 12))
+                                .foregroundColor(.white)
+                            Text("23-29 Aug 2024")
+                                .font(.custom("FKGroteskNeueTrial-Regular", size: 10))
+                                .foregroundColor(.yellow)
+                        }
+                        
+                    }
+                    
+                    Spacer()
+                    Image(systemName: "info.circle.fill")
+                        .resizable()
+                        .frame(width: 24.0, height: 24.0)
+                        .foregroundColor(.primaryOrange)
+                }
+                .padding()
+            }
+            .background(Color.cardBG)
+            .cornerRadius(16)
+            .padding(14)
+        }
     }
+    
 }
 
 #Preview {
