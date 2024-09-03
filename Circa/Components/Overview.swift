398b45120ef89b113caf22fcf6e964bb294d98c0
Akindunjoye <tolulope@busha.co>
2024-09-03 21:16:32 +0100

    overview

---
+++ Circa/Components/Overview.swift
@@ -0,0 +1,18 @@
+//
+//  Overview.swift
+//  Circa
+//
+//  Created by Busha Digital Ltd on 03/09/2024.
+//
+
+import SwiftUI
+
+struct Overview: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Overview()
+}
