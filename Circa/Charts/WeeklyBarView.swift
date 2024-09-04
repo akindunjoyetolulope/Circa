80d245efd4f230fa000f025b0199c679916815d3
hellodamii <daremiarchs@gmail.com>
2024-09-04 08:20:54 +0100

    maybe this will show more updates

--- Circa/Charts/WeeklyBarView.swift
+++ Circa/Charts/WeeklyBarView.swift
@@ -6,12 +6,43 @@
 //
 
 import SwiftUI
+import Charts
+
 
 struct WeeklyBarView: View {
+    
+    let data = [WeeklySummary(date: "Fri", time: 3.1),
+                WeeklySummary(date: "Sat", time: 2.0),
+                WeeklySummary(date: "Sun", time: 2.5),
+                WeeklySummary(date: "Mon", time: 1.0),
+                WeeklySummary(date: "Tue", time: 2.8),
+                WeeklySummary(date: "Wed", time: 4.0),
+                WeeklySummary(date: "Thu", time: 2.0)
+                ]
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Chart {
+            ForEach(data) { dataPoint in
+                
+                BarMark(x: .value("Date", dataPoint.date), y: .value("Time", dataPoint.time))
+                    .cornerRadius(8)
+            }
+        }
+        .chartYScale(domain: 0...8)
+        .foregroundStyle(.primaryOrange)
+        .aspectRatio(contentMode: .fit)
+        
     }
 }
+
+
+struct WeeklySummary: Identifiable {
+    let date: String
+    let time: Double
+    
+    let id = UUID()
+}
+
+
 
 #Preview {
     WeeklyBarView()
